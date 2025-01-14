72f91f4663e4a398d6c2a3ef063d1ccaaef014c6
BriboSD <Gschwinden23@gmail.com>
2025-01-13 17:30:06 -0800

    more setup stuff
    added components that will end up being for channels and channelList down the line

---
+++ client/src/components/channelContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const channelContainer = () => {
+    return (
+        <div>
+            channelContainer 
+        </div>
+
+    )
+}
+
+export default ChannelContainer
---
+++ client/src/components/channelListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const channelListContainer = () => {
+    return (
+        <div>
+            channelListContainer 
+        </div>
+
+    )
+}
+
+export default channelListContainer
